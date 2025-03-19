4a668285cb25184a988f5521ede1ca1f1189a056
jorgin <jorgeigor123@hotmail.com>
2025-03-19 09:57:49 -0300

    Start study logical operators

---
+++ loop.py
@@ -0,0 +1,28 @@
+# for i in range(10):
+#    print(i)
+
+"""c = 0
+while c < 10:
+    print(c)
+    c += 1"""
+    
+'''for i in range(10):
+    if i == 5:
+        continue
+    print(i)'''
+
+"""arr = [1,2,3,4,5,6,7,8,9,10]
+arr_names = ["Joao", "ZÉ", "Marcos"]
+for i in arr_names:
+    print(i)"""
+
+''' OPERADORES LÓGICOS'''
+
+x = 0 
+y = 20
+"""if x> 0 and y > 0:
+    print("Ambos x e y são positivos")"""
+if x > 0 or y > 0:
+    print("Pelo menos x ou y é positivo")
+if not x > 0:
+    print("X não é maior que 0")
---
+++ soma.py
@@ -0,0 +1,14 @@
+import math 
+
+print(round(2.9))
+print(math.pi)
+print(abs(-2.9))
+print(pow(2, 3))
+print(max(2, 3))
+print(min(2, 3))
+print(sum([1, 2, 3]))
+print(chr(65))
+print(ord('A'))
+print(divmod(10, 3))
+print(math.ceil(2.1))
+print(math.floor(2.9))
